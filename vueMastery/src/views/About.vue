25116dc5903b6c155b21828302a9eaacb255682b
Said <zitouni.sd@gmail.com>
2021-09-10 15:13:46 +0100

    finish html css, move to js review

---
+++ vueMastery/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>A site for event for a better world</h1>
+  </div>
+</template>
---
+++ vueMastery/src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+<h1>Events for Good</h1>
+  <div class="events">
+    <event-card v-for='event in events' :key="event.id" :event="event"></event-card>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import EventCard from '../components/EventCard.vue'
+import EventService from '../services/EventService.js'
+export default {
+  name: "Home",
+  data: function(){
+    return {
+    events: null 
+    }
+  },
+  created: function() {
+    EventService.getEvents()
+    .then(response =>{this.events = response.data ; console.log(this.events)})
+    .catch(error => {console.log(error)})
+    
+  },
+  components: {
+    EventCard
+  }
+};
+</script>
+<style lang="css" scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
