75e08ba2bbbc8e509cedd2f1340bd5b1014e37bc
Said ZITOUNI <zitouni.sd@gmail.com>
2021-10-07 18:19:39 +0100

    start product list and bind properties in class to template of the component

---
+++ frameworks/Angular/APM-Start/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products/products-list/products-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frameworks/Angular/APM-Start/src/app/products/products-list/products-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent implements OnInit {
+  pageTitle:String = "Products List";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  products:any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    }
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
