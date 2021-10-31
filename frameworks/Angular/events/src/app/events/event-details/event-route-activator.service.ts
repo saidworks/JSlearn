12d1332dd354264ad07efa693f43c486c9e89417
Said ZITOUNI <zitouni.sd@gmail.com>
2021-10-31 23:20:24 +0100

    partly solved guard problem

--- frameworks/Angular/events/src/app/events/event-details/event-route-activator.service.ts
+++ frameworks/Angular/events/src/app/events/event-details/event-route-activator.service.ts
@@ -2,22 +2,29 @@ import { Injectable } from '@angular/core';
 import { CanActivate , ActivatedRouteSnapshot, Router } from '@angular/router';
 import {EventService} from '../../shared/services/event-service.service';
 import { EventDetailsComponent } from './event-details.component';
-
+import { Subscription } from 'rxjs';
 @Injectable(
   //{providedIn: 'root'}
 )
 export class EventRouteActivatorService implements CanActivate {
-
+  sub?: Subscription;
   constructor(private eventService:EventService, private router:Router) { }
   canActivate(route:ActivatedRouteSnapshot):any{
-    const eventExists = !!this.eventService.getEvent(route.params['id']);
-    if(!eventExists){
-      this.router.navigate(['/404']);
-      console.log('it does not exists')
 
+    this.sub = this.eventService.getEvent(Number(route.params['id'])).subscribe({
+      next:response => {
+         const eventExists = !!response.find(event => event.id === Number(route.params['id']));
+         if(!eventExists){
+          this.router.navigate(['/404']);
+
+
+        }
+        console.log('it exists')
+        return eventExists;
+      }
+    });
+
+    //const eventExists = !!this.eventService.getEvent(route.params['id']);
     }
-    console.log('it exists')
-    return eventExists;
-  }
-  
+
 }
