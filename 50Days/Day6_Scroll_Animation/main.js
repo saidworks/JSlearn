1e477c95b3bda5c6e2ab886c8cd240f281b07551
Said ZITOUNI <zitouni.sd@gmail.com>
2022-04-08 20:00:55 -0400

    add js animation on scroll

--- 50Days/Day6_Scroll_Animation/main.js
+++ 50Days/Day6_Scroll_Animation/main.js
@@ -1,2 +1,17 @@
 const boxes = document.querySelectorAll('.box');
 
+window.addEventListener('scroll', boxesAppear);
+
+function boxesAppear(){
+    const triggerbottom = window.innerHeight*0.5;
+    boxes.forEach(box=>{
+        const boxTop = box.getBoundingClientRect().top;
+        if(boxTop<triggerbottom){
+          box.classList.add('show')  
+        }
+        else{
+            box.classList.remove('show')
+        }
+    })
+
+}
