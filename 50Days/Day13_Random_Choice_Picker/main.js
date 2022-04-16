49579f2445621e7718cc53bace7fd560d73de637
Said ZITOUNI <zitouni.sd@gmail.com>
2022-04-15 22:09:28 -0400

    create choice tags from textarea input

--- 50Days/Day13_Random_Choice_Picker/main.js
+++ 50Days/Day13_Random_Choice_Picker/main.js
@@ -1,3 +1,26 @@
-const tags = document.querySelector('#tags');
-const textArea = document.getElementById('textarea')
+const choicesEl = document.querySelector('#choices');
+const textArea = document.getElementById('textarea');
+
+textArea.focus();
+
+textArea.addEventListener('keyup', (e) => {
+    createTags(e.target.value);
+
+})
+
+function createTags(input){
+    /* we split by comma to create an area, then we use filter with condition on avoinding void word , then we use map to create a new area without whitespaces using trim*/
+    const choices = input.split(',').filter(choice => choice.trim() !== "" )
+    .map(choice => choice.trim());
+    choicesEl.innerHTML = "";
+    choices.forEach(choice =>{
+        const choiceEl = document.createElement('div');
+        choiceEl.setAttribute('class','choice');
+        choiceEl.innerHTML = choice;
+        choicesEl.appendChild(choiceEl);
+    })
+
+
+}
+
 
