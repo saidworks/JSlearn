44865c8514c089f5436dd7383fdf243c603100b0
Said ZITOUNI <zitouni.sd@gmail.com>
2022-04-18 22:16:03 -0400

    add logic to fill big cup, and dynamic fill to big and cup liters tested for scalablity from 2L to 2.5L

--- 50Days/Day16_Drink_Water/main.js
+++ 50Days/Day16_Drink_Water/main.js
@@ -2,10 +2,11 @@ const smallcups = document.querySelectorAll('.cup-small');
 const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
-
-
+const litersQuantity = 2.5; 
+updateBigCup();
 smallcups.forEach((cup,idx)=>{
     cup.addEventListener('click',()=> fillCups(idx))
+    
 })
 
 const fillCups = (idx) =>{
@@ -21,4 +22,37 @@ const fillCups = (idx) =>{
             cup.classList.remove('full');
         }
     })
+    updateBigCup();
+}
+
+function updateBigCup(){
+  //another solution
+    let percentageFill = 0;
+    smallcups.forEach((cup)=>{
+        if(cup.classList.contains('full')){
+            percentageFill += 1/smallcups.length*100
+            let remaining =100- percentageFill
+            percentage.style.height = `${percentageFill}%`;
+            remained.style.height = `${remaining}%`;
+            percentage.innerText = `${percentageFill}%`;
+            //remained.innerText = `${remaining}% remained`;
+            liters.innerText = `${remaining*litersQuantity/100}L `;
+            
+        }
+    })
+    // similar to tutorial
+    // const fullCups = document.querySelectorAll('.cup-small.full').length;
+    // const totalCups = smallcups.length;
+
+    // if(fullCups===0){
+    //     percentage.style.visibility = 'hidden';
+    //     percentage.style.height = 0;
+    // }
+    // else{
+    //     percentage.style.visibility = 'visible';
+    //     percentage.style.height = `${fullCups/totalCups*40}rem`;
+    //     remained.style.height = `${(totalCups-fullCups)/totalCups*40}rem`;
+    //     percentage.innerText = `${fullCups/totalCups*100}%`;
+    //     remained.innerText = `${(totalCups-fullCups)/totalCups*100}% remained`;
+    // }
 }
