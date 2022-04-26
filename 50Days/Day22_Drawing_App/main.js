bb44a4c84fc28627983a1553220cc87da730eed4
Said ZITOUNI <zitouni.sd@gmail.com>
2022-04-25 22:38:37 -0400

    add canvas logic draw cirlce and line

--- 50Days/Day22_Drawing_App/main.js
+++ 50Days/Day22_Drawing_App/main.js
@@ -1,2 +1,26 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+let radius = 20;
+let color = "black";
+let x;
+let y;
+
+
+
+function drawCircle(x,y){
+    ctx.beginPath();
+    //draw a circle  https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+function drawLine(x1,y1,x2,y2){
+    ctx.beginPath();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke();
+}
+
